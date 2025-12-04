1b6935ed72c3ddc0d4ed8752715af9e642113217
Steffen <steffaeaeaen@gmail.com>
2025-12-04 18:35:39 +0100

    create Common.kt

---
+++ src/main/kotlin/common/Common.kt
@@ -0,0 +1,15 @@
+package common
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+val LINE_SEPARATOR: String = System.lineSeparator()
+
+fun loadInput(day: Int): String {
+    return Files.readString(Paths.get("src/main/resources/inputs/day${day.toString().padStart(2, '0')}.txt")).trimIndent()
+}
+
+fun loadTestInput(day: Int): String {
+    return Files.readString(Paths.get("src/main/resources/test_inputs/day${day.toString().padStart(2, '0')}.txt")).trimIndent()
+}
+
